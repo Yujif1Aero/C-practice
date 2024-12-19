f35a4227dd086b2d1b6e25d5d87404f4e6c746a7
Yuji,Sam,Shimojima <shimojima@morgenrot.net>
2024-12-19 21:50:27 +0100

    tiding up directory. add template_function

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# C-practice
+# C and C++ -practice
+## environment
+```
+gcc (Ubuntu 12.3.0-1ubuntu1~22.04) 12.3.0
+Copyright (C) 2022 Free Software Foundation, Inc.
+This is free software; see the source for copying conditions.  There is NO
+warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+```
+g++ (Ubuntu 12.3.0-1ubuntu1~22.04) 12.3.0
+Copyright (C) 2022 Free Software Foundation, Inc.
+This is free software; see the source for copying conditions.  There is NO
+warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+```
---
+++ template_function/test.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+
+template <auto Func>
+void callFunction(int a, int b){
+  std::cout << "callFunction : " << Func(a, b) << std::endl;
+}
+
+template <typename Func>
+void template_callFunction(Func func,int a, int b){
+  std::cout << "template_callFunction : " << func(a,b) << std::endl;
+}
+
+template <auto Func>
+void template_callFunction(int a, int b){
+  std::cout << "template_callFunction without func : " << Func(a,b) << std::endl;
+}
+
+template <auto Func,typename... Args>
+void template_callFunction(Args&&... args){
+  std::cout << "template_callFunction without func and args : " << Func(std::forward<Args>(args)...) << std::endl;
+
+}
+
+
+
+struct Adder {
+  int operator()(int a, int b) {
+    std::cout << "Adderoperator() : " << a+b << std::endl;
+    return a + b;
+  }
+  int operator()(int a, int b) const  {
+    std::cout << "Adder operator() : " << a+b << std::endl;
+    return a + b;
+  }
+};
+struct Adder_args {
+  template <typename... Args>
+    int operator()(Args&&... args) {
+//ys         int result = (0 + ... + args); // 可変長テンプレート引数を畳み込み演算
+    int result = ( ... + args); // 可変長テンプレート引数を畳み込み演算
+    std::cout << "Adder_args operator() : " << result << std::endl;
+    return result;
+    }
+   template <typename... Args>
+    int operator()(Args&&... args)const {
+//ys         int result = (0 + ... + args); // 可変長テンプレート引数を畳み込み演算
+    int result = ( ... + args); // 可変長テンプレート引数を畳み込み演算
+    std::cout << "Adder_args operator() : " << result << std::endl;
+    return result;
+    }
+};
+struct Descending {
+  bool operator()(int a, int b) {
+    return a > b;
+  }
+};
+
+
+int add(int a, int b){
+  return a + b;
+}
+
+int main (){
+  
+  callFunction<add>(1, 2); // pointer of function as template parameter
+  Adder{} (3,4);
+  template_callFunction<Adder>(Adder{},8, 2); // pointer of templete function as template parameter
+  template_callFunction<Adder{}>(5, 2); // pointer of templete function as template parameter
+  template_callFunction<Adder_args{}>(5, 2,1); // pointer of templete function as template parameter
+ 
+  return 0;
+}
